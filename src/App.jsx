6ef558f9222c72ff732d97040c631ca7871db489
PunkMachine <alex_rassydihin@mail.ru>
2022-11-11 10:12:57 +0600

    Сделал верстку сравнения цветов.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,47 @@
+import React from 'react';
+
+// TODO: mask hex
+// TODO: rgb and rgba
+// TODO: add new color to comparison
+// TODO: write color in query
+
 function App() {
 	return (
-		<h1 className="text-3xl font-bold underline">
-			Hello world!
-		</h1>
+		<div className='h-screen flex flex-col items-center justify-center'>
+			<h2 className='mb-6 text-2xl font-bold'>Сравните цвета</h2>
+			<div className='flex justify-center items-center gap-6'>
+				<div className="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
+					<div className='w-full h-24 mb-4 bg-gray-400'></div>
+					<label
+						for="hex1"
+						className="block mb-2 text-sm font-medium text-gray-900"
+					>
+						HEX code
+					</label>
+					<input
+						type="text"
+						id="hex1"
+						className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+						placeholder="#"
+					/>
+				</div>
+				<div className="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
+					<div className='w-full h-24 mb-4 bg-gray-400'></div>
+					<label
+						for="hex2"
+						className="block mb-2 text-sm font-medium text-gray-900"
+					>
+						HEX code
+					</label>
+					<input
+						type="text"
+						id="hex2"
+						className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+						placeholder="#"
+					/>
+				</div>
+			</div>
+		</div>
 	);
 }
 
