b891a8d4a192db41ce03289bef5ef832171a8efc
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2022-11-20 16:13:02 +0600

    Добавил значение по умолчанию аргументу useToggle.

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,6 +1,6 @@
 import { useCallback, useState } from 'react';
 
-export const useToggle = (initialState) => {
+export const useToggle = (initialState = false) => {
 	const [isToggled, setIsToggled] = useState(initialState);
 
 	const toggle = useCallback(() => setIsToggled(!isToggled));
